ee79ae4ff60ecc4a288668e1ae2441e7aeef53ab
eveey connolly <eveey.connolly@my.jcu.edu.au>
2018-08-14 20:39:06 +1000

    Add password checker

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """ Eveey Connolly """
+MIN_PASSWORD_LENGTH = 3
+password = input("Enter Password:")
+
+while len(password) < MIN_PASSWORD_LENGTH:
+    password = input("Enter Password:")
+
+for i in range(0, len(password)):
+    print("*", end="")
